6f981da5b5d546a0ed13acb16a7f2810542e82df
demetri7 <demetri7@msu.edu>
2023-12-09 23:49:51 -0600

    fix url encoding and not running at startup

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: '1',
-        title: 'Search Image',
-        contexts: ["image"],
-    });
-    chrome.contextMenus.onClicked.addListener((info, tab) => {
-        if (!info.srcUrl) return;
-        var url = "https://lens.google.com/uploadbyurl?url=" + info.srcUrl
+chrome.runtime.onStartup.addListener(() => {
+    chrome.contextMenus.removeAll(() => {
+        chrome.contextMenus.create({
+            id: "SearchImage",
+            title: "Search Image",
+            contexts: ["image"],
+        });
 
-        chrome.tabs.create({ url: url });
+        chrome.contextMenus.onClicked.addListener((info, tab) => {
+            if (!info.srcUrl) {
+                return;
+            }
+            var url = "https://lens.google.com/uploadbyurl?url=" + encodeURIComponent(info.srcUrl)
+
+            chrome.tabs.create({ url: url });
+        });
     });
 });
