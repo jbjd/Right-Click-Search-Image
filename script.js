149bd3d16e528fdf0837c1ad0ca4b258b47f5314
jbjd <jimdemetris@yahoo.com>
2023-12-30 00:21:44 -0600

    Finally found fix for flakiness

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-chrome.runtime.onStartup.addListener(() => {
-    chrome.contextMenus.removeAll(() => {
-        chrome.contextMenus.create({
-            id: "SearchImage",
-            title: "Search Image",
-            contexts: ["image"],
-        });
-
-        chrome.contextMenus.onClicked.addListener((info, tab) => {
-            if (!info.srcUrl) {
-                return;
-            }
-            var url = "https://lens.google.com/uploadbyurl?url=" + encodeURIComponent(info.srcUrl)
-
-            chrome.tabs.create({ url: url });
-        });
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "SearchImage",
+        title: "Search Image",
+        contexts: ["image"],
     });
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (!info.srcUrl) {
+        return;
+    }
+    var url = "https://lens.google.com/uploadbyurl?url=" + encodeURIComponent(info.srcUrl)
+
+    chrome.tabs.create({ url: url });
 });
